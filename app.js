449e51c1255b6b66ac7b0407875d77d5177aa736
brittanya <m.brittanya@gmail.com>
2015-07-11 12:12:59 -0600

    updated app & index to modify the inventory

---
+++ app.js
@@ -0,0 +1,44 @@
+var app = angular.module('app', []);
+
+app.service('inventory', ['$http', function ($http) {
+	this.getInventory = function(){
+		return $http.get('http://localhost:3000/posts')
+	}
+
+	this.deleteInventory = function(id){
+		$http.delete('http://localhost:3000/posts/'+id);
+	}
+
+	this.updateInventory = function(upItem, id){
+		$http.put('http://localhost:3000/posts/'+id, upItem);
+	}
+
+	this.createInventory = function(newItem){
+		$http.post('http://localhost:3000/posts', newItem);
+	}
+}])
+
+app.controller('myCtrl', ['$scope', 'inventory', function ($scope, inventory) {
+	inventory.getInventory().success(function(data){
+		$scope.inventory = data;
+	});
+
+	$scope.updatedItem = {};
+
+	$scope.newItem = {};
+
+	$scope.delete = function(id){
+		inventory.deleteInventory(id);
+	};
+
+	$scope.update = function(){
+		inventory.updateInventory($scope.updatedItem, $scope.updatedItem.id);
+		$scope.updatedItem = {};
+	}
+
+	$scope.create = function(){
+		inventory.createInventory($scope.newItem);
+		$scope.newItem = {};
+
+	}
+}])
